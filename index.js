10e770375d0b4f2c160d651a7b812523cfee3471
MikeAdu <mike_adusah@yahoo.com>
2021-09-08 23:18:10 -0700

    added connection, requried my sql and inquirer

---
+++ index.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const inquirer = require("inquirer");
+const table = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "EmployeeTrackerDB"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    askQuestions();
+});
